4f663863df0f6ea9b64849c6376d1f16041d85f1
aaronsantos <aaronpedro16@gmail.com>
2018-02-13 21:14:16 -0300

    Adicionando script de funções de análise qualitativa

---
+++ FONTE/script3_funcoes_analise_quali.R
@@ -0,0 +1,17 @@
+#Funções de análises descritivas
+
+geraAnaliseQuali = function(lista,nome,caminho)
+{
+  tabela = table(lista)
+  
+  caminhoCsv = paste(caminho,"/",nome,".csv",sep="")
+  caminhoPng = paste(caminho,"/",nome,".png",sep="")
+  
+  #write.csv(tabela,caminhoCsv,row.names = FALSE)
+  
+  png(file=caminhoPng, width = 800, height = 800 )
+  pie(tabela)
+  dev.off()
+
+}
+
